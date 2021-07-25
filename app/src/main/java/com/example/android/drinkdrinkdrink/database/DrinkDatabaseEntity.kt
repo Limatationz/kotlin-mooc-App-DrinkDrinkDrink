faf8ea637a542e13980ed848e932dbe7fa186e4e
erik <er99@family-rill.de>
2021-07-25 16:41:55 +0200

    Lösung Repository

--- app/src/main/java/com/example/android/drinkdrinkdrink/database/DrinkDatabaseEntity.kt
+++ app/src/main/java/com/example/android/drinkdrinkdrink/database/DrinkDatabaseEntity.kt
@@ -10,7 +10,7 @@ import java.util.*
  * Eintrag der Datenbank [DrinkDatabase]
  */
 @Entity
-class DrinkDatabaseEntity (
+data class DrinkDatabaseEntity (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val volume: Int,
